5d50cada647544d156e820fd5118728e19130f1e
Xiyuan <xiyuan.tyler@gmail.com>
2025-04-16 12:35:21 -0700

    feat: add env files to the repo

--- notification-service/src/main/java/org/slotify/notificationservice/config/AwsSesConfig.java
+++ notification-service/src/main/java/org/slotify/notificationservice/config/AwsSesConfig.java
@@ -3,19 +3,11 @@ package org.slotify.notificationservice.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.ses.SesAsyncClient;
 
 @Configuration
 public class AwsSesConfig {
-    @Value("${spring.cloud.aws.credentials.access-key}")
-    private String accessKey;
-
-    @Value("${spring.cloud.aws.credentials.secret-key}")
-    private String secretKey;
-
     @Value("${spring.cloud.aws.region.static}")
     private String region;
 
@@ -23,8 +15,6 @@ public class AwsSesConfig {
     public SesAsyncClient sesAsyncClient() {
         return SesAsyncClient.builder()
                 .region(Region.of(region))
-                .credentialsProvider(StaticCredentialsProvider.create(
-                        AwsBasicCredentials.create(accessKey, secretKey)))
                 .build();
     }
 }
