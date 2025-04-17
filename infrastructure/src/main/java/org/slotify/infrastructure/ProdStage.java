5f83d48b7da09e842e6cbc05b6241ee4be541e19
Xiyuan <xiyuan.tyler@gmail.com>
2025-04-16 20:23:43 -0700

    feat: fix minor bug

--- infrastructure/src/main/java/org/slotify/infrastructure/ProdStage.java
+++ infrastructure/src/main/java/org/slotify/infrastructure/ProdStage.java
@@ -18,9 +18,9 @@ public class ProdStage extends Stage {
         ServiceInfo apiGatewayServiceInfo = new ServiceInfo("api-gateway", 8084, null, null);
         ServiceInfo authServiceInfo = new ServiceInfo("auth-service", 8082, null, null);
         ServiceInfo userServiceInfo = new ServiceInfo("user-service", 8081, 9001, "userDB");
-        ServiceInfo slotServiceInfo = new ServiceInfo("slot-service", 8080, 9002, "slotDbStack");
-        ServiceInfo openHourServiceInfo = new ServiceInfo("open-hour-service", 8083, null, "openHourDbStack");
-        ServiceInfo emailTokenServiceInfo = new ServiceInfo("email-token-service", 8086, 9003, "emailTokenDbStack");
+        ServiceInfo slotServiceInfo = new ServiceInfo("slot-service", 8080, 9002, "slotDb");
+        ServiceInfo openHourServiceInfo = new ServiceInfo("open-hour-service", 8083, null, "openHourDb");
+        ServiceInfo emailTokenServiceInfo = new ServiceInfo("email-token-service", 8086, 9003, "emailTokenDb");
         ServiceInfo notificationServiceInfo = new ServiceInfo("notification-service", 8085, null, null);
 
         StackProps stackProps = StackProps.builder()
@@ -37,12 +37,12 @@ public class ProdStage extends Stage {
 
         SecurityGroupStack securityGroupStack = new SecurityGroupStack(this, "security-group-stack", stackProps, vpc, apiGatewayServiceInfo, authServiceInfo, userServiceInfo, slotServiceInfo, openHourServiceInfo, emailTokenServiceInfo, notificationServiceInfo);
 
-        DatabaseStack useDbStack = new DatabaseStack(this, "slotify-" + userServiceInfo.getDbName(), stackProps, vpc, userServiceInfo.getDbName());
-        DatabaseStack slotDbStack = new DatabaseStack(this, "slotify-" + slotServiceInfo.getDbName(), stackProps, vpc, slotServiceInfo.getDbName());
-        DatabaseStack openHourDbStack = new DatabaseStack(this, "slotify-" + openHourServiceInfo.getDbName(), stackProps, vpc, openHourServiceInfo.getDbName());
-        DatabaseStack emailTokenDbStack = new DatabaseStack(this, "slotify-" + emailTokenServiceInfo.getDbName(), stackProps, vpc, emailTokenServiceInfo.getDbName());
+        DatabaseStack userDbStack = new DatabaseStack(this, "slotify-" + userServiceInfo.getDbName() + "-stack", stackProps, vpc, userServiceInfo.getDbName());
+        DatabaseStack slotDbStack = new DatabaseStack(this, "slotify-" + slotServiceInfo.getDbName() + "-stack", stackProps, vpc, slotServiceInfo.getDbName());
+        DatabaseStack openHourDbStack = new DatabaseStack(this, "slotify-" + openHourServiceInfo.getDbName() + "-stack", stackProps, vpc, openHourServiceInfo.getDbName());
+        DatabaseStack emailTokenDbStack = new DatabaseStack(this, "slotify-" + emailTokenServiceInfo.getDbName() + "-stack", stackProps, vpc, emailTokenServiceInfo.getDbName());
 
-        useDbStack.getNode().addDependency(securityGroupStack);
+        userDbStack.getNode().addDependency(securityGroupStack);
         slotDbStack.getNode().addDependency(securityGroupStack);
         openHourDbStack.getNode().addDependency(securityGroupStack);
         emailTokenDbStack.getNode().addDependency(securityGroupStack);
@@ -81,7 +81,7 @@ public class ProdStage extends Stage {
                 stackProps,
                 ecsCluster,
                 List.of(userServiceInfo.getServicePort(), userServiceInfo.getGRPCPort()),
-                useDbStack.getDb(),
+                userDbStack.getDb(),
                 userServiceInfo,
                 Map.of(
                         "SLOT_SERVICE_ADDRESS", slotServiceInfo.getServiceAddress(),
@@ -90,7 +90,7 @@ public class ProdStage extends Stage {
         );
 
         userServiceStack.getNode().addDependency(securityGroupStack);
-        userServiceStack.getNode().addDependency(useDbStack);
+        userServiceStack.getNode().addDependency(userDbStack);
 
         ServiceStack slotServiceStack = new ServiceStack(
                 this,
